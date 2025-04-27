9a5d411d2ca5f4e6a63a275a66bd9f5a167d5b21
inxeoz <inxeoz@gmail.com>
2025-04-27 21:10:01 +0530

    text area

--- src/util/Input.svelte
+++ src/util/Input.svelte
@@ -1,10 +1,15 @@
+<script>
+
+    let normal_text = "";
+
+</script>
+
 <div class="input_main global_center_div">
 
-    <div class="input_text global_border global_font global_center_div">
-        <label for="normal_text">
-            Enter text to convert into phonetic
-        </label>
-    </div>
+    <textarea class="input_text global_border global_font global_center_div"
+              bind:value={normal_text}
+              placeholder="Enter text to convert into phonetic">
+    </textarea>
 
     <div class="convert_button global_border global_font global_center_div">
         <h3>
@@ -16,21 +21,29 @@
 
 
 <style>
-    .input_main{
+    .input_main {
         height: auto;
         width: 100%;
         flex-direction: row;
         column-gap: 20px;
     }
 
-    .input_text{
+
+
+    .input_text {
         width: 500px;
         height: 60px;
         background: #ddb892;
         color: #8a6060;
         text-decoration: underline;
         text-underline-offset: 10px;
+        text-align: center;
+        line-height: 60px;
+
     }
+
+
+
 
     .convert_button {
         width: 150px;
